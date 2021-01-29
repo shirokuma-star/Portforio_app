17833fbb037908d0d51cd75b8c0fc8beec24bd06
Kento Takano <takakenhowaken@gmail.com>
2021-01-29 07:42:13 +0000

    signup and login dekita

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  	root 'static_pages#top'
+  	root 'static_pages#login'
   	get  '/signup', to: 'users#new'
-  	get '/login', to:'static_pages#top'
+  	get '/login', to:'static_pages#login'
   	
   	resources :users
 end
