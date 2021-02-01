5cd8c32abe75e0a840d545a172bd7c490b231453
Kento Takano <takakenhowaken@gmail.com>
2021-02-01 06:59:45 +0000

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to all_path
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -14,5 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			# log_in @user
+			log_in @user
 			flash[:success] = "ようこそ、ポートフォリオの森へ"
 			redirect_to all_path
 		else
