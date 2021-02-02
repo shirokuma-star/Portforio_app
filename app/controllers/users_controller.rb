1ff7b5836dcaa319e88129f76bdf0e5fadf911ed
Kento Takano <takakenhowaken@gmail.com>
2021-02-02 02:50:23 +0000

    Finish login wake

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:all, :show]
+	before_action :correct_user,   only: [:show]
 	
 	def show
 		@user = User.find(params[:id])
@@ -30,4 +32,24 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                   :password_confirmation)
     end
+    
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください"
+        redirect_to root_url
+      end
+    end
+    
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user)
+        flash[:danger] = "閲覧権限がありません"
+        redirect_to all_path
+      end
+    end
+    
 end
